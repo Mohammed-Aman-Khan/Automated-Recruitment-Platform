f40182bead3cb4bddf5b9d48deb172f0000c9ac0
Mohammed-Aman-Khan <45879026+Mohammed-Aman-Khan@users.noreply.github.com>
2021-12-23 20:52:03 +0530

    Angular binding with Electron

--- app/main.ts
+++ app/main.ts
@@ -1,16 +1,19 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var electron_1 = require("electron");
-var path = require("path");
-var fs = require("fs");
-var url = require("url");
-var win = null;
-var args = process.argv.slice(1), serve = args.some(function (val) { return val === '--serve'; });
-function createWindow() {
-    var electronScreen = electron_1.screen;
-    var size = electronScreen.getPrimaryDisplay().workAreaSize;
+import { app, BrowserWindow, screen } from 'electron'
+import * as path from 'path'
+import * as fs from 'fs'
+import * as url from 'url'
+
+let win: BrowserWindow = null
+const args = process.argv.slice(1),
+    serve = args.some(val => val === '--serve')
+
+function createWindow(): BrowserWindow {
+
+    const electronScreen = screen
+    const size = electronScreen.getPrimaryDisplay().workAreaSize
+
     // Create the browser window.
-    win = new electron_1.BrowserWindow({
+    win = new BrowserWindow({
         x: 0,
         y: 0,
         width: size.width,
@@ -18,62 +21,69 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true,
             allowRunningInsecureContent: (serve) ? true : false,
-            contextIsolation: false, // false if you want to run e2e test with Spectron
+            contextIsolation: false,  // false if you want to run e2e test with Spectron
         },
-    });
+    })
+
+
     if (serve) {
-        win.webContents.openDevTools();
+        win.webContents.openDevTools()
         require('electron-reload')(__dirname, {
             electron: require(path.join(__dirname, '/../node_modules/electron'))
-        });
-        win.loadURL('http://localhost:4200');
-    }
-    else {
+        })
+        win.loadURL('http://localhost:4200')
+    } else {
         // Path when running electron executable
-        var pathIndex = './index.html';
+        let pathIndex = './index.html'
+
         if (fs.existsSync(path.join(__dirname, '../dist/index.html'))) {
             // Path when running electron in local folder
-            pathIndex = '../dist/index.html';
+            pathIndex = '../dist/index.html'
         }
+
         win.loadURL(url.format({
             pathname: path.join(__dirname, pathIndex),
             protocol: 'file:',
             slashes: true
-        }));
+        }))
     }
+
     // Emitted when the window is closed.
-    win.on('closed', function () {
+    win.on('closed', () => {
         // Dereference the window object, usually you would store window
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        win = null;
-    });
-    return win;
+        win = null
+    })
+
+    return win
 }
+
 try {
     // This method will be called when Electron has finished
     // initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
     // Added 400 ms to fix the black background issue while using transparent window. More detais at https://github.com/electron/electron/issues/15947
-    electron_1.app.on('ready', function () { return setTimeout(createWindow, 400); });
+    app.on('ready', () => setTimeout(createWindow, 400))
+
     // Quit when all windows are closed.
-    electron_1.app.on('window-all-closed', function () {
+    app.on('window-all-closed', () => {
         // On OS X it is common for applications and their menu bar
         // to stay active until the user quits explicitly with Cmd + Q
         if (process.platform !== 'darwin') {
-            electron_1.app.quit();
+            app.quit()
         }
-    });
-    electron_1.app.on('activate', function () {
+    })
+
+    app.on('activate', () => {
         // On OS X it's common to re-create a window in the app when the
         // dock icon is clicked and there are no other windows open.
         if (win === null) {
-            createWindow();
+            createWindow()
         }
-    });
-}
-catch (e) {
+    })
+
+} catch (e) {
     // Catch Error
     // throw e;
 }
-//# sourceMappingURL=main.js.map
