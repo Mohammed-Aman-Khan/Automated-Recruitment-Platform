626b2b978c33d3eb87c9b9b7e3a9560917694d2e
Mohammed-Aman-Khan <45879026+Mohammed-Aman-Khan@users.noreply.github.com>
2021-12-26 20:41:31 +0530

    Configured @angular/material

--- src/app/detail/detail.module.ts
+++ src/app/detail/detail.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { DetailRoutingModule } from './detail-routing.module';
+import { DetailRoutingModule } from './detail-routing.module'
 
-import { DetailComponent } from './detail.component';
-import { SharedModule } from '../shared/shared.module';
+import { DetailComponent } from './detail.component'
+import { SharedModule } from '../shared/shared.module'
+
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
-  declarations: [DetailComponent],
-  imports: [CommonModule, SharedModule, DetailRoutingModule]
+    declarations: [DetailComponent],
+    imports: [CommonModule, SharedModule, DetailRoutingModule, MatButtonModule]
 })
-export class DetailModule {}
+export class DetailModule { }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { HomeRoutingModule } from './home-routing.module';
+import { HomeRoutingModule } from './home-routing.module'
 
-import { HomeComponent } from './home.component';
-import { SharedModule } from '../shared/shared.module';
+import { HomeComponent } from './home.component'
+import { SharedModule } from '../shared/shared.module'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+    declarations: [HomeComponent],
+    imports: [CommonModule, SharedModule, HomeRoutingModule, MatButtonModule]
 })
-export class HomeModule {}
+export class HomeModule { }
