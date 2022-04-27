a8551e617a39683c5a10b24657c3470623b53c1d
Manavpreeet <manavpreeetsingh@gmail.com>
2022-04-27 20:13:35 +0530

    Added JobSeekerWorker skeleton functions

--- classes/JobSeekerWorker.ts
+++ classes/JobSeekerWorker.ts
@@ -1,6 +1,175 @@
-import { JobSeekerWorkerInterface } from '../interfaces/JobSeekerWorker.interface'
+import { GetJobsResponse, GetMyDetailsResponse, JobFilter, JobSeekerWorkerInterface } from '../interfaces/JobSeekerWorker.interface'
+import { BasicResponse } from '../interfaces/Shared.interface';
+import JobSeeker from '../schemas/JobSeeker.schema'
+import { EmailId } from '../types/Credential.types'
+import { CurrentlyEmployed, Experience, Interests, Qualification, Skill } from '../types/JobSeeker.types';
+import { MongoObjectId } from '../types/Shared.types'
+import { JobSeekerInterface } from "../interfaces/JobSeeker.interface"
+
 
 class JobSeekerWorker implements JobSeekerWorkerInterface {
+
+    async getJobs (authId: MongoObjectId, filter?: JobFilter): Promise<GetJobsResponse> {
+        try {
+
+
+
+            return {
+                status: true,
+                jobs: []
+    
+            }
+            
+        } catch (error) {
+            return {
+                status: false,
+                error: error.message,
+                jobs: []
+            }
+        }
+    }
+
+// Facing error here
+    // async getMyDetails (authId: MongoObjectId, email: EmailId): Promise<GetMyDetailsResponse> {
+    //     try {
+    //         return {
+    //             data: JobSeekerInterface,
+    //             status: true
+    //         }
+    //     } catch (error) {
+    //         return {
+    //             error: error.message,
+    //             status: false,
+    //             data: JobSeekerInterface
+    //         }
+    //     }
+    // }
+
+    async addQualification (authId: MongoObjectId, newQualification: Qualification): Promise<BasicResponse> {
+        try {
+            return {
+                error: "",
+                status: true
+            }
+        } catch (error) {
+            return {
+                error: error.message,
+                status: false
+            }
+        }
+    }
+
+    async removeQualification (authId: MongoObjectId, index: number): Promise<BasicResponse> {
+        try {
+            return {
+                error: '',
+                status: true
+            }
+        } catch (error) {
+            return{
+                error: error.message,
+                status: false
+            }
+        }
+    }
+
+    async addExperience (authId: MongoObjectId, newExperience: Experience): Promise<BasicResponse> {
+        try {
+            return {
+                error: '',
+                status: true
+            }
+        } catch (error) {
+            return {
+                error: error.message,
+                status: false
+            }
+        }
+    }
+
+    async removeExperience (authId: MongoObjectId, index: number): Promise<BasicResponse> {
+        try {
+            return {
+                status: true,
+                error: ''
+            }
+        } catch (error) {
+            return {
+                status: false,
+                error: error.message
+            }
+        }
+    }
+
+    async addSkill (authId: MongoObjectId, newSkill: Skill): Promise<BasicResponse> {
+        try {
+            return {
+                status: true,
+                error: ''
+            }
+        } catch (error) {
+            return {
+                error: error.message,
+                status: false
+            }
+        }
+    }
+
+    async removeSkill (authId: MongoObjectId, index: number): Promise<BasicResponse> {
+        try {
+            return {
+                status: true,
+                error: ''
+            }
+        } catch (error) {
+            return {
+                status: false,
+                error: error.message
+            }
+        }
+    }
+
+    async updateInterests (authId: MongoObjectId, updatedInterests: Interests[]): Promise<BasicResponse> {
+        try {
+            return {
+                status: true,
+                error: ''
+            }
+        } catch (error) {
+            return  {
+                status: false,
+                error: error.message
+            }
+        }
+    }
+
+    async setCurrentlyEmployed (authId: MongoObjectId, currentlyEmployed: CurrentlyEmployed): Promise<BasicResponse> {
+        try {
+            return {
+                status: true,
+                error: ''
+            }
+        } catch (error) {
+            return  {
+                status: false,
+                error: error.message
+            }
+        }
+    }
+
+    async applyForJob (authId: MongoObjectId, jobId: MongoObjectId): Promise<BasicResponse> {
+        try {
+            return {
+                status: true,
+                error: ''
+            }
+        } catch (error) {
+            return  {
+                status: false,
+                error: error.message
+            }
+        }
+    }
 
 }
 
